f8262c2e531fb5ca7085e6db4c5cf1c4bf4db440
OuHaixiong <258333309@163.com>
2015-09-30 16:25:02 +0800

    图片类

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
 <?php
+
+defined('ROOT_PATH') || define('ROOT_PATH', realpath(dirname(__FILE__)));
+defined('LIBRARIES_PATH') || define('LIBRARIES_PATH', realpath(ROOT_PATH . '/libraries')); // 后面不带 /
+
+include_once LIBRARIES_PATH . '/Common/Abstract.php';
+include_once LIBRARIES_PATH . '/Common/Image.php';
+include_once LIBRARIES_PATH . '/Common/Imagick.php';
+
+
